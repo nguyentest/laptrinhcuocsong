a51275b2710f2d47211a333a3ac3c98c4eb9ae25
Bùi văn nguyện <phongcachmaytinh@gmail.com>
2017-11-03 02:20:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-console.log("Ngày quay tay mấy lần ?", 'font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;font-size:24px;color:#00bbee;-webkit-text-fill-color:#00bbee;-webkit-text-stroke: 1px #00bbee;', "font-size:12px;color:#999999;");
+console.log("%c Ngày quay tay mấy lần ? %c", 'font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;font-size:24px;color:#00bbee;-webkit-text-fill-color:#00bbee;-webkit-text-stroke: 1px #00bbee;', "font-size:12px;color:#999999;");
